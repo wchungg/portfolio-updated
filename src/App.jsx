436433a68132f85f90a66ea692953a7928b7a7c9
William Chung <williamchung@ucsb.edu>
2026-03-13 23:38:42 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1>Welcome to my portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
